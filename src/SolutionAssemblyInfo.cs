6c4d7afdda0038718f1bcce6b1c56fbb40196269
Geert van Horrik <geert@catenalogic.com>
2014-04-15 11:02:16 +0200

    Initial solution setup

---
+++ src/SolutionAssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SolutionAssemblyInfo.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+#pragma warning disable 1699    // 1699 = Use command line option '/keyfile' or appropriate project settings instead of 'AssemblyKeyFile'
+
+using System;
+using System.Reflection;
+using System.Resources;
+
+// Shared assembly info that is common for all assemblies of this project
+
+////[assembly: AssemblyTitle("DEFINED IN ACTUAL ASSEMBLYINFO")]
+////[assembly: AssemblyProduct("DEFINED IN ACTUAL ASSEMBLYINFO")]
+////[assembly: AssemblyDescription("DEFINED IN ACTUAL ASSEMBLYINFO")]
+
+[assembly: AssemblyCompany("CatenaLogic")]
+[assembly: AssemblyCopyright("Copyright © CatenaLogic 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyInformationalVersion("1.0, manual release in Visual Studio")]
+
+// CLS compliant
+#if !NETFX_CORE && !XAMARIN
+[assembly: CLSCompliant(true)]
+#endif
