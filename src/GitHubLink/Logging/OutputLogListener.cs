b86ce697b00bcfca660a52d967f228433c5f8a15
Geert van Horrik <geert@catenalogic.com>
2014-08-27 15:37:26 +0200

    Updated all NuGet packages

--- src/GitHubLink/Logging/OutputLogListener.cs
+++ src/GitHubLink/Logging/OutputLogListener.cs
@@ -7,6 +7,7 @@
 
 namespace GitHubLink.Logging
 {
+    using System;
     using Catel.Logging;
 
     public class OutputLogListener : ConsoleLogListener
@@ -17,7 +18,14 @@ namespace GitHubLink.Logging
             IsDebugEnabled = true;
         }
 
-        protected override string FormatLogEvent(ILog log, string message, LogEvent logEvent, object extraData)
+        /// <summary>
+        /// Formats the log event to a message which can be written to a log persistence storage.
+        /// </summary>
+        /// <param name="log">The log.</param><param name="message">The message.</param><param name="logEvent">The log event.</param><param name="extraData">The extra data.</param><param name="time">The time.</param>
+        /// <returns>
+        /// The formatted log event.
+        /// </returns>
+        protected override string FormatLogEvent(ILog log, string message, LogEvent logEvent, object extraData, DateTime time)
         {
             return message;
         }
