4d9e19a42937c22fcdc8a3fcb3a20ca3ede300b8
Cameron MacFarland <distantcam@gmail.com>
2016-02-14 12:54:34 +0800

    Added GitLink task project

---
+++ src/GitLinkTask/LinkProject.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace GitLinkTask
+{
+    public class LinkProject : ToolTask
+    {
+        [Required]
+        public ITaskItem SolutionDirectory { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        protected override string ToolName => "GitLink.exe";
+
+        protected override string GenerateFullPathToTool() => Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "GitLink.exe");
+
+        protected override string GenerateCommandLineCommands() => $"{SolutionDirectory.FullPath()} -include {ProjectName}";
+    }
+}
