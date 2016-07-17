eabb48178298689a99adfb864e7ad8d76a75d27d
tpluscode <tpluscode@users.noreply.github.com>
2016-07-17 12:05:11 +0200

    test for selecting projects from sln file

---
+++ src/GitLink.Tests/ProjectHelperFacts.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProjectHelperFacts.cs" company="CatenaLogic">
+//   Copyright (c) 2014 - 2016 CatenaLogic. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GitLink.Tests
+{
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ProjectHelperFacts
+    {
+        private const string SolutionFile = @"TestSolution\TestSolution.sln"; 
+
+        [Test]
+        public void GettingProjectsFromSolution()
+        {
+            Assert.AreEqual(3, ProjectHelper.GetProjects(SolutionFile, "Debug", "Any CPU").Count());
+        }
+    }
+}
