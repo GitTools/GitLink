e56fb70ad66886290a6e990358a9df15af86bba9
Geert van Horrik <geert@catenalogic.com>
2017-01-18 14:44:16 +0100

    Use GitVersionTask

--- src/GitLink/Properties/AssemblyInfo.cs
+++ src/GitLink/Properties/AssemblyInfo.cs
@@ -1,16 +1,12 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="CatenaLogic">
-//   Copyright (c) 2014 - 2016 CatenaLogic. All rights reserved.
+//   Copyright (c) 2014 - 2017 CatenaLogic. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GitLink")]
 [assembly: AssemblyProduct("GitLink")]
 [assembly: AssemblyDescription("GitLink library")]
-
-#if !PCL
-[assembly: ComVisible(false)]
-#endif
--- src/GitLinkTask/Properties/AssemblyInfo.cs
+++ src/GitLinkTask/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
-﻿// Copyright (c) Andrew Arnott. All rights reserved.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="CatenaLogic">
+//   Copyright (c) 2014 - 2017 CatenaLogic. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GitLinkTask")]
 [assembly: AssemblyProduct("GitLinkTask")]
 [assembly: AssemblyDescription("GitLinkTask library")]
-
-#if !PCL
-[assembly: ComVisible(false)]
-#endif
