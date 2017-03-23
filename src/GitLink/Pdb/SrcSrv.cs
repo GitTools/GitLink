d0bf800dbc70ec49b9af798992fe132922dd908e
Jason <v-jacai@qq.com>
2017-03-23 22:16:37 +0800

    fix issue 150

--- src/GitLink/Pdb/SrcSrv.cs
+++ src/GitLink/Pdb/SrcSrv.cs
@@ -35,16 +35,9 @@ namespace GitLink.Pdb
                     sw.WriteLine("RAWURL={0}", CreateTarget(rawUrl, revision));
                     if (downloadWithPowershell)
                     {
-                        if (rawUrl.Contains("?") && !string.IsNullOrEmpty(revision))
-                        {
-                            sw.WriteLine(string.Format("TRGFILE=%targ%\\{0}\\%fnbksl%(%var2%)", revision));
-                        }
-                        else
-                        {
-                            sw.WriteLine("TRGFILE=%fnbksl%(%targ%%var2%)");
-                        }
+                        sw.WriteLine("TRGFILE=%fnbksl%(%targ%%var2%)");
                         sw.WriteLine("SRCSRVTRG=%TRGFILE%");
-                        sw.WriteLine("SRCSRVCMD=powershell invoke-command -scriptblock {param($url='%RAWURL%', $output='%TRGFILE%'); $webClient = New-Object System.Net.WebClient; $webClient.UseDefaultCredentials = $true; $webClient.DownloadFile($url, $output);}");
+                        sw.WriteLine("SRCSRVCMD=powershell invoke-command -scriptblock {$webClient = New-Object System.Net.WebClient; $webClient.UseDefaultCredentials = $true; $webClient.DownloadFile('%RAWURL%', '%TRGFILE%');}");
                     }
                     else
                     {
