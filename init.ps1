e3647d746780184979c5f4677a8479e6922dcc38
Andrew Arnott <andrewarnott@gmail.com>
2017-12-10 19:02:26 -0800

    Remove nuget.exe downloader and nuproj

--- init.ps1
+++ init.ps1
@@ -12,15 +12,8 @@ try {
     $nugetVerbosity = 'quiet'
     if ($Verbose) { $nugetVerbosity = 'normal' }
 
-    # First restore NuProj packages since the solution restore depends on NuProj evaluation succeeding.
-    gci "$PSScriptRoot\src\project.json" -rec |? { $_.FullName -imatch 'nuget' } |% {
-        & "$toolsPath\Restore-NuGetPackages.ps1" -Path $_ -Verbosity $nugetVerbosity
-    }
-
     # Restore VS solution dependencies
-    gci "$PSScriptRoot\src" -rec |? { $_.FullName.EndsWith('.sln') } |% {
-        & "$toolsPath\Restore-NuGetPackages.ps1" -Path $_.FullName -Verbosity $nugetVerbosity
-    }
+    msbuild.exe /t:restore "$PSScriptRoot\src\GitLink.sln"
 
     Write-Host "Successfully restored all dependencies" -ForegroundColor Yellow
 }
