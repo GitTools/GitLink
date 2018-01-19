b71e1cf004398d88bd60bbd74210c50744c3e3bc
jnm2 <me@jnm2.com>
2018-01-19 15:06:00 -0500

    Fix incorrect contraction "let's"

--- src/GitLinkTask/Properties/AssemblyInfo.cs
+++ src/GitLinkTask/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("GitLinkTask")]
 [assembly: AssemblyProduct("GitLink")]
-[assembly: AssemblyDescription("GitLink let's users step through your code hosted on any Git hosting service! This makes symbol servers obsolete which saves you both time")]
+[assembly: AssemblyDescription("GitLink lets users step through your code hosted on any Git hosting service! This makes symbol servers obsolete which saves you both time")]
